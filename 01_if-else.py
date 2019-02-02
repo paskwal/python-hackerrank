c588235bafbdb56e0b863ef23b41a825806be388
Pascal Sitbon <psitbon@gmail.com>
2019-02-02 11:54:46 +0100

    Add solution for 01 If Else

---
+++ 01_if-else.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+
+N = int(input())
+
+if (N>100 or N <1):
+    exit(0)
+if (N % 2 != 0) # N is odd:
+    print("Weird")
+else:
+    if (N<=5 and N>=2):
+        print("Not Weird")
+    elif (N>=6 and N<=20):
+        print("Weird")
+    else:
+        print("Not Weird")
+      
+
