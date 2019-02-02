6d5923058cbf69e69f512fdd78bd414e9d81ba24
Pascal Sitbon <psitbon@gmail.com>
2019-02-02 16:48:01 +0100

    Added 04 and 05 exercises

---
+++ 04_loops.py
@@ -0,0 +1,12 @@
+# Task 
+# Read an integer . For all non-negative integers , print . See the sample for details.
+
+if __name__ == '__main__':
+    n = int(input())
+
+if n > 20:
+    exit(0)
+
+for i in range(0,n):
+    print(i**2)
+
